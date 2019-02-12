7bffc309121490a443d0508aa44e3fb74c1fe9f6
elimerrell <elijah.merrell@gmail.com>
2019-02-12 11:55:15 -0800

    Setup tables for each migration, added column names and datatypes

--- db/migrate/20190212194018_create_branches.rb
+++ db/migrate/20190212194018_create_branches.rb
@@ -1,4 +1,8 @@
 class CreateBranches < ActiveRecord::Migration[5.0]
   def change
+    create_table :branches do |t|
+      t.string :name
+      t.string :location
+      t.integer :company_id 
   end
 end
--- db/migrate/20190212194033_create_companies.rb
+++ db/migrate/20190212194033_create_companies.rb
@@ -1,4 +1,9 @@
 class CreateCompanies < ActiveRecord::Migration[5.0]
   def change
+    create_table :companies do |t|
+      t.string :name
+      t.text :description
+      t.string :company_url
+      t.string :logo_url
   end
 end
