06e477ffcf344f466e1875492b0af9eda357593d
elimerrell <elijah.merrell@gmail.com>
2019-02-12 16:46:18 -0800

    Starting working on search functionality, completed user input section for search

--- bin/cli.rb
+++ bin/cli.rb
@@ -2,7 +2,7 @@ class CLI
 
 def welcome
    puts "Welcome to Dev Job Hunter! Have you already signed up (y/n)?"
-   sign_up_response = gets.chomp
+   sign_up_response = gets.chomp.downcase
    if sign_up_response == 'y'
      main_menu
    elsif sign_up_response == 'n'
@@ -15,13 +15,15 @@ end
 
 def sign_up
   puts "Welcome to Dev Job Hunter!"
+  puts
   puts "Please enter your name"
-  hunter_name = gets.chomp
+  hunter_name = gets.chomp.downcase
   puts "Welcome, #{hunter_name}!"
+  puts
   puts "Please enter the tecnhologies you are fluent in i.e. 'ruby,java,javascript' "
-  hunter_tecnhologies = gets.chomp
+  hunter_tecnhologies = gets.chomp.downcase
   puts "Please enter your current location"
-  hunter_location = gets.chomp
+  hunter_location = gets.chomp.downcase
 
   #put this here for now to delete previous users
   JobHunter.destroy_all
@@ -33,14 +35,79 @@ def sign_up
     )
 
   puts "Thanks for signing up, #{hunter_name}, you can now search developer jobs!"
+  main_menu
 end
 
 def main_menu
   puts "What would you like to do?"
+  puts
   puts "1. Search Developer Jobs"
+  puts
   puts "2. See Saved Jobs"
+  puts
   puts "3. Apply for job"
+  main_menu_response = gets.chomp.downcase
 
+  if main_menu_response[0] == "1"
+    search_jobs
+  elsif main_menu_response[0] == "2"
+    saved_jobs
+  elsif main_menu_response[0] == "3"
+    apply_job
+  end
+
+end
+
+def search_jobs
+   puts "How would you like to search?"
+   puts
+   puts "1. Search by location"
+   puts
+   puts "2. Search by title"
+   puts
+   puts "3. Search by tecnhologies"
+   search_jobs_response = gets.chomp.downcase
+
+   if search_jobs_response[0] == "1"
+     search_by_location
+   elsif search_jobs_response[0] == "2"
+     search_by_title
+   elsif search_jobs_response[0] == "3"
+     search_by_technologies
+   end
+end
+
+def search_by_location
+  puts "Please enter the location where you want to work"
+  user_location_response = gets.chomp.downcase
+  location = JobPosting.find_by(:location.downcase == user_location_response)
+    if location
+      puts "Here are the listings in that area"
+      puts "#{location[:title]}"
+    elsif !location
+      puts "Sorry, there are no openings in that city"
+    end
+end
+
+def search_by_title
+puts "searching by title"
+
+end
+
+def search_by_technologies
+  puts "searching by tecnhologies"
+end
+
+
+def saved_jobs
+
+puts "here are your saved jobs"
+end
+
+
+def apply_job
+
+puts "what would you like to apply for"
 end
 
 
