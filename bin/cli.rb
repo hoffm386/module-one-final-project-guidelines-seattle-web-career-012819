4f297dd80f99cac2d34bab608d389802b4ebebd1
elimerrell <elijah.merrell@gmail.com>
2019-02-13 09:16:15 -0800

    added colorize to cli for formatting strings

--- bin/cli.rb
+++ bin/cli.rb
@@ -1,20 +1,21 @@
+require 'colorize'
 class CLI
 
 def welcome
-   puts "Welcome to Dev Job Hunter! Have you already signed up (y/n)?"
+   puts "Welcome to Dev Job Hunter! Have you already signed up (y/n)?".colorize(:green)
    sign_up_response = gets.chomp.downcase
    if sign_up_response == 'y'
      main_menu
    elsif sign_up_response == 'n'
      sign_up
    else
-     puts "That's not a valid command"
+     puts "That's not a valid command".colorize(:red)
      return self.welcome
    end
 end
 
 def sign_up
-    puts "Welcome to Dev Job Hunter!"
+    puts "Welcome to the Dev Job Hunter sign up!".colorize(:green)
     puts
     puts "Please enter your name"
     hunter_name = gets.chomp.downcase
@@ -34,18 +35,20 @@ def sign_up
       location: hunter_location
       )
 
-    puts "Thanks for signing up, #{hunter_name}, you can now search developer jobs!"
+    puts "Thanks for signing up, #{hunter_name}, you can now search developer jobs!".colorize(:green)
     main_menu
 end
 
 def main_menu
-    puts "What would you like to do?"
     puts
-    puts "1. Search Developer Jobs"
-    puts
-    puts "2. See Saved Jobs"
-    puts
-    puts "3. Apply for job"
+    puts "What would you like to do?".colorize(:green)
+    puts "---------------------"
+    puts "1. Search Developer Jobs".colorize(:color => :light_blue, :background => :white)
+    puts "---------------------"
+    puts "2. See Saved Jobs".colorize(:color => :light_blue, :background => :white)
+    puts "---------------------"
+    puts "3. Apply for job".colorize(:color => :light_blue, :background => :white)
+    puts "---------------------"
     main_menu_response = gets.chomp.downcase
 
     if main_menu_response[0] == "1"
@@ -54,18 +57,22 @@ def main_menu
       saved_jobs
     elsif main_menu_response[0] == "3"
       apply_job
+    else
+      puts "That is not a valid command".colorize(:red)
+      main_menu
     end
-
 end
 
 def search_jobs
-   puts "How would you like to search?"
    puts
-   puts "1. Search by location"
+   puts "How would you like to search?".colorize(:green)
    puts
-   puts "2. Search by title"
-   puts
-   puts "3. Search by tecnhologies"
+   puts "1. Search by location".colorize(:color => :light_blue, :background => :white)
+   puts "---------------------"
+   puts "2. Search by title".colorize(:color => :light_blue, :background => :white)
+   puts "---------------------"
+   puts "3. Search by tecnhologies".colorize(:color => :light_blue, :background => :white)
+   puts "---------------------"
    search_jobs_response = gets.chomp.downcase
 
    if search_jobs_response[0] == "1"
@@ -74,33 +81,38 @@ def search_jobs
      search_by_title
    elsif search_jobs_response[0] == "3"
      search_by_technologies
+   else
+     puts "That is not a valid command".colorize(:red)
+     search_jobs
    end
 end
 
 def search_by_location
-  puts "Please enter the location where you want to work"
+  puts
+  puts "Please enter the location where you want to work".colorize(:green)
   user_location_response = gets.chomp.downcase
    jobs_by_location = JobPosting.joins(:branch).where('LOWER(branches.location) LIKE ?', "%#{user_location_response}%")
     if jobs_by_location.count > 0
-      puts "Here are the jobs in your area:"
+      puts "Here are the jobs in your area:".colorize(:green)
       puts
       puts jobs_by_location.each_with_index.map {|job,index| puts "#{index + 1}. #{job[:title]}"}
     else
-      puts "Sorry, there are no jobs in your area"
+      puts "Sorry, there are no jobs in your area".colorize(:red)
       search_by_location
     end
 end
 
 def search_by_title
-  puts "Please enter the job title you would like to search"
+  puts
+  puts "Please enter the job title you would like to search".colorize(:green)
   user_job_title_response = gets.chomp.downcase
    jobs_by_title = JobPosting.where("job_postings.title LIKE ?", "%#{user_job_title_response}%")
   if jobs_by_title.count > 0
-    puts "Here are the current openings with similar titles:"
+    puts "Here are the current openings with similar titles:".colorize(:green)
     puts
     puts jobs_by_title.each_with_index.map {|job,index| puts "#{index + 1}. #{job[:title]}"}
   else
-    puts "Sorry, there are no jobs that match this title"
+    puts "Sorry, there are no jobs that match this title".colorize(:red)
     search_by_title
   end
 end
