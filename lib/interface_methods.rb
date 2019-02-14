e38eea9abbfa0ccd4dcbc9ec31fbd79d7427747e
github username <github email address>
2019-02-13 17:27:08 -0800

    Refactoring interface code (first commit of task).

--- lib/interface_methods.rb
+++ lib/interface_methods.rb
@@ -1,5 +1,71 @@
 #for fun!
+class UserInterface
+    attr_reader :authors, :books, :publishers, :book_deals
+
+    def initialize(authors, books, publishers, book_deals)
+        @authors = authors
+        @books = books
+        @publishers = publishers
+        @book_deals = book_deals
+    end
+
+    # A string to search for, a symbol for the dataset to look in,
+    # and an attribute of the items in that dataset to look in.
+
+    #self.search("Allen", "authors", "name") # by author
+    #self.search("Hal", "publishers", "name") # by publisher
+    #self.search("Hal", "publishers", "name", "books") # return the books
+
+    # def self.search(search_term, class_name, attrib_to_check, attrib_to_return=nil)
+    #     return_array = []
+    #     self[class_name].each do |class_instance|
+    #         if class_instance[attrib_to_check].class == Array
+    #             class_instance[attrib_to_check].each do |attrib_we_checked|
+    #                 if attrib_we_checked.to_s.downcase.include?(search_term.to_s.downcase)
+    #                     value_to_shovel = attrib_we_checked
+    #                     if attrib_to_return 
+    #                         value_to_shovel = class_instance[attrib_to_return]
+    #                     end
+    #                     return_array << value_to_shovel
+    #                 end
+    #             end
+    #         else
+    #             if class_instance[attrib].to_s.downcase.include?(search_term.to_s.downcase)
+    #                 return_array << class_instance[attrib]
+    #             end
+    #         end
+    #     end
+    #     return_array
+    # end
+    def results(ans)
+        ans.each do |book|
+            puts "#{book.title}, by #{book.authors[0].name}."
+         end
+         puts "\n"
+         ans
+    end
+
+    def find_book_by_title(str)
+        ans = self.books.select do |book|
+            book.title.downcase.include?(str.downcase)
+        end
+        results(ans)
+    end
+
+    # def self.find_books_by_author(author_to_find)
+    #     self.search(author_to_find, "authors", "name")
+    # end
+
+    # def self.find_books_by_publisher(publisher_to_find)
+    #     self.search(publisher_to_find, "publishers", "name", "books")
+    # end
+end
+
+menu = UserInterface.new(Author.all, Book.all, Publisher.all, BookDeal.all)
+binding.pry
 # class Interface
+
+# generic find, what to look for, what attribute to find, where to find it.
 
 def find_all_book_titles
     arr = []
@@ -9,37 +75,37 @@ def find_all_book_titles
     arr
 end
 
-def find_book_by_title(str)
-    ans = Book.all.select do |book|
-        book.title.downcase == str.downcase
-    end
-    if ans.empty?
-        "That's not a book, bro."
-    else
-    "Your book is #{ans[0].title}, by #{ans[0].authors[0].name}."
-    end
-    puts "\n"
-    ans
-end
+# def find_book_by_title(str)
+#     ans = Book.all.select do |book|
+#         book.title.downcase == str.downcase
+#     end
+#     if ans.empty?
+#         "That's not a book, bro."
+#     else
+#     "Your book is #{ans[0].title}, by #{ans[0].authors[0].name}."
+#     end
+#     puts "\n"
+#     ans
+# end
 
-def books_by_author(str)
-    ans = Book.all.select do |book|
-        book.authors[0].name.downcase.include?(str.downcase)
-    end
-    puts "This author has written or collaborated on: "
-        ans.each do |book|
-       puts "#{book.title}, by #{book.authors[0].name}."
-    end
-    puts "\n"
-    ans
-end
+# def books_by_author(str)
+#     ans = Book.all.select do |book|
+#         book.authors[0].name.downcase.include?(str.downcase)
+#     end
+#     puts "This author has written or collaborated on: "
+#         ans.each do |book|
+#        puts "#{book.title}, by #{book.authors[0].name}."
+#     end
+#     puts "\n"
+#     ans
+# end
 
 def books_by_publisher(str)
     ans = Book.all.select do |book|
         book.publishers[0].name.downcase.include?(str.downcase)
     end
     puts "This publisher has released: "
-        ans.each do |book|
+    ans.each do |book|
        puts "#{book.title}, by #{book.authors[0].name}."
     end
     puts "\n"
--- lib/models/publisher.rb
+++ lib/models/publisher.rb
@@ -1,4 +1,5 @@
 class Publisher < ActiveRecord::Base
+  has_many :book_deals
   has_many :authors, through: :book_deals
   has_many :books, through: :book_deals
 end
