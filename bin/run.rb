4793c9a90407fc276a7a64d3741a1d85879af0bf
github username <github email address>
2019-02-14 15:29:28 -0800

    Made Trivia questions working

--- bin/run.rb
+++ bin/run.rb
@@ -5,7 +5,8 @@ require_relative "../lib/API/command_line_interface.rb"
 #call methods here
 CLI = Command_line_interface.new
 
+#CLI.question_01
+#CLI.test
 CLI.books_by_publisher
-
 # binding.pry
 # 0
--- lib/API/command_line_interface.rb
+++ lib/API/command_line_interface.rb
@@ -1,21 +1,65 @@
 class Command_line_interface
-
+  @stars = 0
   def welcome
     puts "********* Welcome to the Library Trivia Game *********"
     puts "(Please choose the correct answers for each questions)"
   end
 
+  def test
+    author_name = []
+    Creator.all.each do |author|
+      author_name << author.name
+    end
+    puts author_name.sample
+  end
+
   def question_01
+    choice01 = "David Baldacci"
     puts "1. Which author has written the most books?"
-    print ""
+    puts
+    puts "    a. #{choice01}.  b. Emma Healey.  c.Garry Disher"
+    puts
+    print "Please select an answer: "
     answer = gets.chomp
+    most_authors = nil
+    book_count =0
+    Creator.all.each do |creator|
+      if creator.books.count > book_count
+        book_count = creator.books.count
+        most_authors = creator.name
+      end
+    end
+    if answer == "a" || answer == "A"
+      puts "*************************************************************"
+      puts "*************************************************************"
+      puts
+      puts "*****Correct!! Congratulations******"
+      puts "*****#{most_authors} has written #{book_count} books******"
+    else
+      puts "Incorrect, please try again"
+      puts "*************************************************************"
+      puts "*************************************************************"
+      puts
+      puts
+      self.question_01
+    end
   end
 
   def books_by_publisher
-    print "Enter a publisher name: "
-    publisher_name = gets.chomp
-    publisher = Publisher.find_by name: publisher_name
-    puts publisher.books.map {|book| "#{book.name} | #{book.creator.name}"}
+    print "Which publisher has most authors?"
+    #publisher_name = gets.chomp
+    # publisher = Publisher.find_by name: publisher_name
+    # puts publisher.books.map {|book| "#{book.name} | #{book.creator.name}"}
+    author_count =0
+    publisher_name = nil
+    Publisher.all.each do |publisher|
+      if publisher.creators.count > author_count
+        author_count = publisher.creators.count
+        publisher_name = publisher.name
+      end
+    end
+    puts "#{author_count} and #{publisher_name}"
+
   end
 
 end #end of class
