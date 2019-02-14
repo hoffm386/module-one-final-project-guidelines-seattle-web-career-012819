e3693fff1f92e45fe0d235cb69c9aaaa3b8038d6
Tiffany <Tafby88@gmail.com>
2019-02-13 17:14:36 -0800

    added battle and trainer class

---
+++ db/migrate/006_create_trainer.rb
@@ -0,0 +1,14 @@
+class CreateTrainer < ActiveRecord::Migration 
+    def change
+        create_table :trainers do |t|
+           t.string :name
+           t.string :flavor_text
+           t.integer :p1
+           t.integer :p2
+           t.integer :p3
+           t.integer :p4
+           t.integer :p1
+           t.integer :p6
+        end
+    end
+end
