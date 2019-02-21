9e9c9893a974f78f0973d934914f4f59ab57a4ff
hoffm386 <erinrhof@uw.edu>
2019-02-21 15:53:58 -0800

    Adjusted whitespace for readability
    Some lines were really long, others weren't lined up based on their scopes

--- bin/api_communicator.rb
+++ bin/api_communicator.rb
@@ -4,39 +4,65 @@ require 'pry'
 
 
 def get_pokemon_from_api
-seed_games
-response_string = RestClient.get('https://pokeapi.co/api/v2/pokemon/?limit=20')
-response_hash = JSON.parse(response_string)
-#Seeding Pokemon
-response_hash["results"].each do |pokemon|
+  seed_games
+  response_string = RestClient.get('https://pokeapi.co/api/v2/pokemon/?limit=20')
+  response_hash = JSON.parse(response_string)
+  #Seeding Pokemon
+  response_hash["results"].each do |pokemon|
     pokemon_url_string = RestClient.get(pokemon["url"])
     pokemon_url_hash = JSON.parse(pokemon_url_string)
     #creating the pokemon object
     if (pokemon_url_hash["types"].size == 1)
-      Pokemon.create(name: pokemon["name"].downcase, url: pokemon["url"], weight: pokemon_url_hash["weight"], height: pokemon_url_hash["height"], type1: pokemon_url_hash["types"][0]["type"]["name"], hp: pokemon_url_hash["stats"].last["base_stat"])
+      Pokemon.create(
+        name: pokemon["name"].downcase,
+        url: pokemon["url"],
+        weight: pokemon_url_hash["weight"],
+        height: pokemon_url_hash["height"],
+        type1: pokemon_url_hash["types"][0]["type"]["name"],
+        hp: pokemon_url_hash["stats"].last["base_stat"]
+      )
     else
-      Pokemon.create(name: pokemon["name"].downcase, url: pokemon["url"], weight: pokemon_url_hash["weight"], height: pokemon_url_hash["height"], type1: pokemon_url_hash["types"][0]["type"]["name"], type2: pokemon_url_hash["types"][1]["type"]["name"], hp: pokemon_url_hash["stats"].last["base_stat"])
+      Pokemon.create(
+        name: pokemon["name"].downcase,
+        url: pokemon["url"],
+        weight: pokemon_url_hash["weight"],
+        height: pokemon_url_hash["height"],
+        type1: pokemon_url_hash["types"][0]["type"]["name"],
+        type2: pokemon_url_hash["types"][1]["type"]["name"],
+        hp: pokemon_url_hash["stats"].last["base_stat"]
+      )
     end
     #creating the associations for the games
     pokemon_url_hash["game_indices"].each do |x|
-      PokemonGame.create(pokemon_id: Pokemon.last.id, game_id: Game.find_by(name: x["version"]["name"].downcase).id)
+      PokemonGame.create(
+        pokemon_id: Pokemon.last.id,
+        game_id: Game.find_by(name: x["version"]["name"].downcase).id
+      )
     end
     #creating the moves
     pokemon_url_hash["moves"].each do |m|
       name = m["move"]["name"]
       pokemon_move_string = RestClient.get(m["move"]["url"])
       pokemon_move_hash = JSON.parse(pokemon_move_string)
-      Move.create(name: name, accuracy: pokemon_move_hash["accuracy"], pp: pokemon_move_hash["pp"], damage: pokemon_move_hash["power"], move_type: pokemon_move_hash["type"]["name"] )
-      PokemonMove.create(pokemon_id: Pokemon.last.id, move_id: Move.last.id)
+      Move.create(
+        name: name,
+        accuracy: pokemon_move_hash["accuracy"],
+        pp: pokemon_move_hash["pp"],
+        damage: pokemon_move_hash["power"],
+        move_type: pokemon_move_hash["type"]["name"]
+      )
+      PokemonMove.create(
+        pokemon_id: Pokemon.last.id,
+        move_id: Move.last.id
+      )
 
     end
     #grabbing the default 4 moves per pokemon
-      Pokemon.all.last.update_attribute(:move1, Pokemon.all.last.moves.sample.id)
-      Pokemon.all.last.update_attribute(:move2, Pokemon.all.last.moves.sample.id)
-      Pokemon.all.last.update_attribute(:move3, Pokemon.all.last.moves.sample.id)
-      Pokemon.all.last.update_attribute(:move4, Pokemon.all.last.moves.sample.id)
-    end
-
+    Pokemon.all.last.update_attribute(:move1, Pokemon.all.last.moves.sample.id)
+    Pokemon.all.last.update_attribute(:move2, Pokemon.all.last.moves.sample.id)
+    Pokemon.all.last.update_attribute(:move3, Pokemon.all.last.moves.sample.id)
+    Pokemon.all.last.update_attribute(:move4, Pokemon.all.last.moves.sample.id)
+  end
 end
 
 
@@ -73,8 +99,44 @@ def seed_games
 end
 
 def seed_trainers
-Trainer.create(name: "Ash", flavor_text: "I'm having a major hat crisis!", p1: Pokemon.all.sample.id, p2: Pokemon.all.sample.id, p3: Pokemon.all.sample.id, p4: Pokemon.all.sample.id, p5: Pokemon.all.sample.id, p6: Pokemon.all.sample.id)
-Trainer.create(name: "Brock", flavor_text: "Hey! I know! I'll use my trusty frying pan as a drying pan!", p1: Pokemon.all.sample.id, p2: Pokemon.all.sample.id, p3: Pokemon.all.sample.id, p4: Pokemon.all.sample.id, p5: Pokemon.all.sample.id, p6: Pokemon.all.sample.id)
-Trainer.create(name: "Misty", flavor_text: "Pikachu! You're a Pika Pal!", p1: Pokemon.all.sample.id, p2: Pokemon.all.sample.id, p3: Pokemon.all.sample.id, p4: Pokemon.all.sample.id, p5: Pokemon.all.sample.id, p6: Pokemon.all.sample.id)
-Trainer.create(name: "Jessie + James", flavor_text: "Prepare for trouble! And make it double! To protect the world from devestation! To Unite all peoples within our nation! To denounce the evils for truth and love! To extend our reach to the stars above! Jessie! James! Team Rocket blasts off at the speed of light! Surrender now, or prepare to fight! Meowth: That's right!", p1: Pokemon.all.sample.id, p2: Pokemon.all.sample.id, p3: Pokemon.all.sample.id, p4: Pokemon.all.sample.id, p5: Pokemon.all.sample.id, p6: Pokemon.all.sample.id)
+  Trainer.create(
+    name: "Ash",
+    flavor_text: "I'm having a major hat crisis!",
+    p1: Pokemon.all.sample.id,
+    p2: Pokemon.all.sample.id,
+    p3: Pokemon.all.sample.id,
+    p4: Pokemon.all.sample.id,
+    p5: Pokemon.all.sample.id,
+    p6: Pokemon.all.sample.id
+  )
+  Trainer.create(
+    name: "Brock",
+    flavor_text: "Hey! I know! I'll use my trusty frying pan as a drying pan!",
+    p1: Pokemon.all.sample.id,
+    p2: Pokemon.all.sample.id,
+    p3: Pokemon.all.sample.id,
+    p4: Pokemon.all.sample.id,
+    p5: Pokemon.all.sample.id,
+    p6: Pokemon.all.sample.id
+  )
+  Trainer.create(
+    name: "Misty",
+    flavor_text: "Pikachu! You're a Pika Pal!",
+    p1: Pokemon.all.sample.id,
+    p2: Pokemon.all.sample.id,
+    p3: Pokemon.all.sample.id,
+    p4: Pokemon.all.sample.id,
+    p5: Pokemon.all.sample.id,
+    p6: Pokemon.all.sample.id
+  )
+  Trainer.create(
+    name: "Jessie + James",
+    flavor_text: "Prepare for trouble! And make it double! To protect the world from devestation! To Unite all peoples within our nation! To denounce the evils for truth and love! To extend our reach to the stars above! Jessie! James! Team Rocket blasts off at the speed of light! Surrender now, or prepare to fight! Meowth: That's right!",
+    p1: Pokemon.all.sample.id,
+    p2: Pokemon.all.sample.id,
+    p3: Pokemon.all.sample.id,
+    p4: Pokemon.all.sample.id,
+    p5: Pokemon.all.sample.id,
+    p6: Pokemon.all.sample.id
+  )
 end
--- bin/cli.rb
+++ bin/cli.rb
@@ -1,105 +1,105 @@
 require "pry"
 
 class CLI
-@@pokedex_array = []
+  @@pokedex_array = []
 
-    def pokedex_array
-      @@pokedex_array
-    end
+  def pokedex_array
+    @@pokedex_array
+  end
 
-    def run
-    # puts "Seeding Databases..."
-    # #get_pokemon_from_api()
-    # seed_trainers
-    # puts "Seeding Complete"
+  def run
+    puts "Seeding Databases..."
+    get_pokemon_from_api()
+    seed_trainers
+    puts "Seeding Complete"
     welcome
     choice = options
-        while choice != 4
-            option_answer(choice)
-            choice = options
-        end
+    while choice != 4
+      option_answer(choice)
+      choice = options
     end
+  end
 
-    def welcome
-        puts "Welcome to Alpha Super Ultra Purple!"
-    end
+  def welcome
+    puts "Welcome to Alpha Super Ultra Purple!"
+  end
 
-    def options
-        puts ""
-        puts "Choose an option"
-        puts "1. Catch a Pokemon"
-        puts "2. Pokedex"
-        puts "3. Battle a Trainer!"
-        puts "4. Exit"
-        answer = gets.chomp
-    end
-
-    def option_answer(answer)
-        if answer == "1"
-            catch_pokemons
-        elsif answer == "2"
-            pokedex
-        elsif answer == "3"
-            battle
-        elsif answer == "4"
-            puts "Exiting ..."
-            exit
-        else
-            puts "Incorrect option, please try again!"
-        end
-    end
-
-    def catch_pokemons
-        random_pokemon = Pokemon.all.sample
-        # binding.pry
-        puts ""
-        puts "A wild #{random_pokemon.name} appeared!"
-        puts "Choose one"
-        puts "1. Catch"
-        puts "2. Run"
-        answer = gets.chomp
-        if answer == "1"
-            random_number = rand(1..2)
-            if random_number == 1
-                puts "You caught it! yay!"
-                @@pokedex_array << random_pokemon
-            else
-                puts "Tough luck bub! You missed!"
-            end
-        end
-    end
-
-    def pokedex
+  def options
     puts ""
-        puts "1. Lookup Pokemon"
-        puts "2. View your caught Pokemon"
-        answer = gets.chomp
-        if answer == "1"
-            puts "Please enter a Pokemon name"
-            p_name = gets.chomp
+    puts "Choose an option"
+    puts "1. Catch a Pokemon"
+    puts "2. Pokedex"
+    puts "3. Battle a Trainer!"
+    puts "4. Exit"
+    answer = gets.chomp
+  end
 
-            puts ""
-            puts Pokemon.find_by(name: p_name).print
-            puts ""
-        elsif answer == "2"
-            @@pokedex_array.each do |poke|
-            puts ""
-            puts poke.print
-            puts ""
-            end
-        else
-            puts "Your answer is whack!"
-        end
+  def option_answer(answer)
+    if answer == "1"
+      catch_pokemons
+    elsif answer == "2"
+      pokedex
+    elsif answer == "3"
+      battle
+    elsif answer == "4"
+      puts "Exiting ..."
+      exit
+    else
+      puts "Incorrect option, please try again!"
     end
+  end
 
-    def battle
-      while @@pokedex_array.size <= 6
-        pokeguy = Pokemon.all.sample
-        if @@pokedex_array.include?(pokeguy) == false
-          @@pokedex_array << pokeguy
-        end
+  def catch_pokemons
+    random_pokemon = Pokemon.all.sample
+    # binding.pry
+    puts ""
+    puts "A wild #{random_pokemon.name} appeared!"
+    puts "Choose one"
+    puts "1. Catch"
+    puts "2. Run"
+    answer = gets.chomp
+    if answer == "1"
+      random_number = rand(1..2)
+      if random_number == 1
+        puts "You caught it! yay!"
+        @@pokedex_array << random_pokemon
+      else
+        puts "Tough luck bub! You missed!"
       end
-      b = Battle.new(Trainer.all.sample, self)
-      b.main_battle_loop
     end
+  end
+
+  def pokedex
+    puts ""
+    puts "1. Lookup Pokemon"
+    puts "2. View your caught Pokemon"
+    answer = gets.chomp
+    if answer == "1"
+      puts "Please enter a Pokemon name"
+      p_name = gets.chomp
+
+      puts ""
+      puts Pokemon.find_by(name: p_name).print
+      puts ""
+    elsif answer == "2"
+      @@pokedex_array.each do |poke|
+        puts ""
+        puts poke.print
+        puts ""
+      end
+    else
+      puts "Your answer is whack!"
+    end
+  end
+
+  def battle
+    while @@pokedex_array.size <= 6
+      pokeguy = Pokemon.all.sample
+      if @@pokedex_array.include?(pokeguy) == false
+        @@pokedex_array << pokeguy
+      end
+    end
+    b = Battle.new(Trainer.all.sample, self)
+    b.main_battle_loop
+  end
 end
